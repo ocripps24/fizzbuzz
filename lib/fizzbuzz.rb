b8a3ab95781212032f8ae972b45cc5422fc18dfe
Oliver Cripps <ollycripps@gmail.com>
2019-03-27 11:33:43 +0000

    add alt file and refactor original

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,15 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    return 'fizzbuzz'
-  elsif number % 5 == 0
-    return 'buzz'
-  elsif number % 3 == 0
-    return 'fizz'
-  else
-    return number
-  end
-end
-
 # def fizzbuzz(number)
-#   number % 15 == 0 ? fizzbuzz : number % 5 == 0 ? buzz : number % 3 == 0 ? fizz : number
+#   if number % 3 == 0 && number % 5 == 0
+#     return 'fizzbuzz'
+#   elsif number % 5 == 0
+#     return 'buzz'
+#   elsif number % 3 == 0
+#     return 'fizz'
+#   else
+#     return number
+#   end
 # end
+
+def fizzbuzz(number)
+  number % 15 == 0 ? fizzbuzz : number % 5 == 0 ? buzz : number % 3 == 0 ? fizz : number
+end
