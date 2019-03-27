8d65c93b1e26b20f46cc82a708f6df32e6821c35
Guilherme <guiheurich@gmail.com>
2019-03-27 11:55:47 +0000

    Tested alternate version

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -11,5 +11,5 @@
 # end
 
 def fizzbuzz(number)
-  number % 15 == 0 ? fizzbuzz : number % 5 == 0 ? buzz : number % 3 == 0 ? fizz : number
+  number % 15 == 0 ? "fizzbuzz" : number % 5 == 0 ? "buzz" : number % 3 == 0 ? "fizz" : number
 end
